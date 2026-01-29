a206aff5e84ada759a86dcfcc247d1cd659a88ed
sonmina002-dev <sonmina002@gmail.com>
2026-01-30 01:46:45 +0630

    Update script.js

--- script.js
+++ script.js
@@ -9,33 +9,29 @@ const noTexts = [
 ];
 
 let noCount = 0;
-let scale = 1;
+let musicStarted = false;
+
+function startMusic() {
+    if (!musicStarted) {
+        music.play();
+        musicStarted = true;
+    }
+}
 
 noBtn.addEventListener('click', () => {
+    startMusic(); // Plays music on first click
     noCount++;
     
-    // Change No text
+    // Change No text but DO NOT grow Yes button
     if (noCount < noTexts.length) {
         noBtn.innerText = noTexts[noCount];
     }
-
-    // Grow Yes button
-    scale += 0.3;
-    yesBtn.style.transform = `scale(${scale})`;
-    
-    // Move No button slightly so it doesn't get covered
-    const x = (Math.random() - 0.5) * 60;
-    const y = (Math.random() - 0.5) * 60;
-    noBtn.style.transform = `translate(${x}px, ${y}px)`;
 });
 
 yesBtn.addEventListener('click', () => {
+    startMusic(); // Plays music if they click Yes immediately
     document.getElementById('main-heading').innerHTML = "Yay!!! ❤️";
     document.getElementById('sub-text').style.display = 'none';
     bearImg.src = "https://media.tenor.com/gU_Pb_7p_5YAAAAj/mochi-mochi-peach-cat-cat.gif";
     document.querySelector('.button-wrapper').style.display = 'none';
 });
-
-function playMusic() {
-    music.play();
-}
