2c10220fa9ed031a4e33373ff35d30ff4b1f3bd3
sonmina002-dev <sonmina002@gmail.com>
2026-01-30 01:29:01 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const yesBtn = document.getElementById('yesBtn');
 const noBtn = document.getElementById('noBtn');
-const heading = document.getElementById('main-heading');
+const mainHeading = document.getElementById('main-heading');
+const bearImg = document.getElementById('bear-img');
 const music = document.getElementById('song');
 
-let yesSize = 1;
+const noTexts = [
+    "No", "Are you sure?", "Really sure??", "Think again!", 
+    "Last chance!", "Surely not?", "You might regret this!", 
+    "Give it another thought!", "Are you absolutely sure?", 
+    "This could be a mistake!", "Have a heart!", 
+    "Don't be so cold!", "Change of heart?", 
+    "Is that your final answer?", "You're breaking my heart ;("
+];
 
-// Make the NO button move when hovered or touched
-noBtn.addEventListener('mouseover', moveButton);
-noBtn.addEventListener('touchstart', moveButton);
+let noCount = 0;
 
-function moveButton() {
-    const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
-    const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
-    noBtn.style.left = `${x}px`;
-    noBtn.style.top = `${y}px`;
+noBtn.addEventListener('click', () => {
+    noCount++;
     
-    // Make the YES button grow every time they try to hit NO
-    yesSize += 0.2;
-    yesBtn.style.transform = `scale(${yesSize})`;
-}
+    // 1. Change the text of the No button
+    if (noCount < noTexts.length) {
+        noBtn.innerText = noTexts[noCount];
+    } else {
+        noBtn.innerText = "No :(";
+    }
 
-// What happens when they click YES
+    // 2. Make the Yes button bigger
+    const currentSize = parseFloat(window.getComputedStyle(yesBtn).fontSize);
+    yesBtn.style.fontSize = (currentSize * 1.4) + "px";
+    yesBtn.style.padding = (currentSize * 0.8) + "px " + (currentSize * 1.2) + "px";
+});
+
 yesBtn.addEventListener('click', () => {
-    heading.innerHTML = "Yay! See you soon! ❤️";
+    mainHeading.innerHTML = "Yay!!! ❤️❤️❤️";
+    bearImg.src = "https://media.tenor.com/gU_Pb_7p_5YAAAAj/mochi-mochi-peach-cat-cat.gif"; // Happy cat
     document.querySelector('.buttons').style.display = 'none';
+    document.getElementById('sub-text').style.display = 'none';
 });
 
 function playMusic() {
