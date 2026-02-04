27c1036794ce8bb50d9e8d6e44043b776d4f4acb
sonmina002-dev <sonmina002@gmail.com>
2026-02-04 12:19:11 +0630

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,11 @@ const bearImg = document.getElementById('bear-img');
 const music = document.getElementById('song');
 
 const noTexts = [
-    "No", 
-    "Are you sure?", 
-    "What if I asked really nicely?", 
-    "Pretty please", 
-    "With a chocolate rice cake on top", 
-    "What about a matcha frostie", 
-    "PLEASE POOKIE", 
-    "But :(", 
-    "I am going to die", 
-    "Yep im dead", 
-    "ok ur talking to nathan's ghost", 
-    "please babe", 
-    ":((((", 
-    "PRETTY PLEASE", 
-    "Estoy muerto", 
-    "No :("
+    "No", "Are you sure?", "What if I asked really nicely?", "Pretty please", 
+    "With a chocolate rice cake on top", "What about a matcha frostie", 
+    "PLEASE POOKIE", "But :(", "I am going to die", "Yep im dead", 
+    "ok ur talking to nathan's ghost", "please babe", ":((((", 
+    "PRETTY PLEASE", "Estoy muerto", "No :("
 ];
 
 let noCount = 0;
@@ -27,7 +16,7 @@ let scale = 1;
 
 function startApp() {
     document.getElementById('overlay').style.display = 'none';
-    music.play();
+    music.play().catch(e => console.log("Music play failed:", e));
 }
 
 noBtn.addEventListener('click', () => {
@@ -35,14 +24,12 @@ noBtn.addEventListener('click', () => {
     if (noCount < noTexts.length) {
         noBtn.innerText = noTexts[noCount];
     }
-    // Yes button grows larger with every "No" click
     scale += 0.5;
     yesBtn.style.transform = `scale(${scale})`;
 });
 
 yesBtn.addEventListener('click', () => {
-    document.getElementById('main-heading').innerHTML = "Yay!!! ❤️";
-    document.getElementById('sub-text').style.display = 'none';
+    document.getElementById('sub-text').innerHTML = "Yay!!! ❤️";
     bearImg.src = "https://media.tenor.com/gU_Pb_7p_5YAAAAj/mochi-mochi-peach-cat-cat.gif";
     document.querySelector('.button-wrapper').style.display = 'none';
 });
